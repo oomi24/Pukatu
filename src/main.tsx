0692625c4112bffad23c1894f6d5ce6a49450805
pukatu <oomiapp@gmail.com>
2025-06-28 10:00:55 -0400

    Corrijo tsconfig.json para build correcto en Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 );
+
+// Error TS6305: El archivo de salida '/vercel/path0/vite.config.d.ts' no se ha creado a partir del archivo fuente '/vercel/path0/vite.config.ts'.
+// El archivo está en el programa porque:
+//   Coincide con el patrón de inclusión 'vite.config.ts' en '/vercel/path0/tsconfig.json'
